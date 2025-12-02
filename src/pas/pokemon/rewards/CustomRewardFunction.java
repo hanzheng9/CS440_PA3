491e185665310429fc5229849b5f114d429168d5
Han Zheng <hanzheng@Hans-MacBook-Air-2.local>
2025-12-02 14:17:04 -0500

    finished rewards function and sensor array, working on policy rn

--- src/pas/pokemon/rewards/CustomRewardFunction.java
+++ src/pas/pokemon/rewards/CustomRewardFunction.java
@@ -11,6 +11,7 @@ import edu.bu.pas.pokemon.core.Battle.BattleView;
 import edu.bu.pas.pokemon.core.Move.MoveView;
 import edu.bu.pas.pokemon.core.Pokemon.PokemonView;
 import edu.bu.pas.pokemon.core.Team.TeamView;
+import edu.bu.pas.pokemon.core.enums.Stat;
 
 
 public class CustomRewardFunction
@@ -25,31 +26,85 @@ public class CustomRewardFunction
     public double getLowerBound()
     {
         // TODO: change this. Reward values must be finite!
-        return Double.POSITIVE_INFINITY;
+        return -1.0;
     }
 
     public double getUpperBound()
     {
         // TODO: change this. Reward values must be finite!
-        return Double.NEGATIVE_INFINITY;
+        return 1.0;
     }
 
     public double getStateReward(final BattleView state)
     {
-        return 0d;
+        if(state==null) 
+            return 0.0;
+        TeamView myTeam = state.getTeam1View();
+        TeamView oppTeam = state.getTeam2View();
+        double myHPFrac = getTeamHPFraction(myTeam);
+        double oppHPFrac = getTeamHPFraction(oppTeam);
+        double diff = 0.5*(myHPFrac-oppHPFrac);
+        double terminalBonus = 0.0;
+
+        if(state.isOver())
+        {
+            if(myHPFrac>oppHPFrac)      
+                terminalBonus = 0.5;  
+            else if(myHPFrac<oppHPFrac) 
+                terminalBonus = -0.5; 
+            else                       
+                terminalBonus = 0.0;  
+        }
+
+        double rewards = diff+terminalBonus;
+        if(rewards>getUpperBound()) 
+            rewards = getUpperBound();
+        if(rewards<getLowerBound()) 
+            rewards = getLowerBound();
+
+        return rewards;
+    }
+
+    private double getTeamHPFraction(final TeamView team) 
+    {
+        if(team==null || team.size()==0) 
+            return 0.0;
+        double current = 0.0;
+        double max = 0.0;
+
+        for (int i=0; i<team.size(); i++) 
+        {
+            PokemonView pokemon = team.getPokemonView(i);
+            if(pokemon==null) 
+                continue;
+            int initialHp = pokemon.getInitialStat(Stat.HP);
+            int currentHp = pokemon.getCurrentStat(Stat.HP);
+
+            if(initialHp<0) 
+                initialHp = 0;
+            if(currentHp<0) 
+                currentHp = 0;
+
+            max += initialHp;
+            current += currentHp;
+        }
+
+        if(max<=0.0) 
+            return 0.0;
+
+        return current/max;
     }
 
     public double getStateActionReward(final BattleView state,
                                        final MoveView action)
     {
-        return 0d;
+        return getStateReward(state);
     }
 
     public double getStateActionStateReward(final BattleView state,
                                             final MoveView action,
                                             final BattleView nextState)
     {
-        return 0d;
+        return getStateReward(nextState);
     }
-
 }
--- src/pas/pokemon/senses/CustomSensorArray.java
+++ src/pas/pokemon/senses/CustomSensorArray.java
@@ -11,6 +11,10 @@ import edu.bu.pas.pokemon.core.Move.MoveView;
 import edu.bu.pas.pokemon.core.Pokemon.PokemonView;
 import edu.bu.pas.pokemon.core.Team.TeamView;
 import edu.bu.pas.pokemon.linalg.Matrix;
+import edu.bu.pas.pokemon.core.enums.NonVolatileStatus;
+import edu.bu.pas.pokemon.core.enums.Stat;
+import edu.bu.pas.pokemon.core.enums.Type;
+import edu.bu.pas.pokemon.core.Move.Category;
 
 
 public class CustomSensorArray
@@ -28,9 +32,98 @@ public class CustomSensorArray
     {
         // TODO: Convert a BattleView and a MoveView into a row-vector containing measurements for every sense
         // you want your neural network to have. This method should be called if your model is a q-based model
+        TeamView myTeam = state.getTeam1View();
+        TeamView oppTeam = state.getTeam2View();
+        PokemonView my = myTeam.getActivePokemonView();
+        PokemonView opp = oppTeam.getActivePokemonView();
+        double[] f = new double[64]; // features
+        int i = 0;
 
-        // currently returns 64 random numbers
-        return Matrix.randn(1, 64);
+        // HP
+        f[i++] = my.getCurrentStat(Stat.HP)/(double)my.getInitialStat(Stat.HP);
+        f[i++] = opp.getCurrentStat(Stat.HP)/(double)opp.getInitialStat(Stat.HP);
+
+        // stats
+        f[i++] = my.getCurrentStat(Stat.ATK)/255.0;
+        f[i++] = my.getCurrentStat(Stat.SPATK)/255.0;
+        f[i++] = my.getCurrentStat(Stat.SPD)/255.0;
+        f[i++] = opp.getCurrentStat(Stat.ATK)/255.0;
+        f[i++] = opp.getCurrentStat(Stat.SPATK)/255.0;
+        f[i++] = opp.getCurrentStat(Stat.SPD)/255.0;
+
+        for(Type type: Type.values()) 
+        {
+            double val = 0.0;
+            if(my.getCurrentType1()==type || my.getCurrentType2()==type) 
+                val = 1.0;
+            f[i++] = val;
+        }
+        for(Type type: Type.values()) 
+        {
+            double val = 0.0;
+            if (opp.getCurrentType1()==type || opp.getCurrentType2()==type) 
+                val = 1.0;
+            f[i++] = val;
+        }
+
+        // status
+        double myPar = 0.0;
+        if(my.getNonVolatileStatus()==NonVolatileStatus.PARALYSIS)
+             myPar = 1.0;
+        f[i++] = myPar;
+        double mySleep = 0.0;
+        if (my.getNonVolatileStatus()==NonVolatileStatus.SLEEP) 
+            mySleep = 1.0;
+        f[i++] = mySleep;
+        double myFreeze = 0.0;
+        if (my.getNonVolatileStatus()==NonVolatileStatus.FREEZE) 
+            myFreeze = 1.0;
+        f[i++] = myFreeze;
+        double oppPar = 0.0;
+        if(opp.getNonVolatileStatus()==NonVolatileStatus.PARALYSIS) 
+            oppPar = 1.0;
+        f[i++] = oppPar;
+        double oppSleep = 0.0;
+        if(opp.getNonVolatileStatus()==NonVolatileStatus.SLEEP) 
+            oppSleep = 1.0;
+        f[i++] = oppSleep;
+        double oppFreeze = 0.0;
+        if(opp.getNonVolatileStatus()==NonVolatileStatus.FREEZE) 
+            oppFreeze = 1.0;
+        f[i++] = oppFreeze;
+
+        if(action!=null) 
+        {
+            double power = 0.0;
+            if(action.getPower()!=null) 
+                power = action.getPower()/200.0;
+            f[i++] = power;
+
+            double accuracy = 0.0;
+            if(action.getAccuracy()!=null) 
+                accuracy = action.getAccuracy()/100.0;
+            f[i++] = accuracy;
+
+            double priority = (action.getPriority()+1.0)/2.0;
+            f[i++] = priority;
+
+            double damage = 0.0;
+            if(action.getCategory()!=Category.STATUS) 
+                damage = 1.0;
+            f[i++] = damage;
+        } 
+        else 
+        {
+            f[i++] = 0.0;
+            f[i++] = 1.0;
+            f[i++] = 0.5;
+            f[i++] = 0.0;
+        }
+
+        Matrix m = Matrix.zeros(1, f.length);
+        for(int j=0; j<f.length; j++) 
+            m.set(0, j, f[j]);
+        
+        return m;
     }
-
 }
